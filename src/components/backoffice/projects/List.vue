de76abead6922d7f9702b636c6cdca9cd4ec7e46
Filipe <filipefernandes007@gmail.com>
2019-04-08 21:01:02 +0100

    Work in project : create and list

---
+++ src/components/backoffice/projects/List.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div id="project-list">
+
+
+    </div>
+
+</template>
+
+<script type="text/javascript">
+    export default {
+        el: 'project-list',
+        name: 'Project',
+        data() {
+            return {
+                project: {
+                    id: null,
+                    name: '',
+                    title: '',
+                    description: ''
+                }
+            }
+        },
+        created() {
+
+
+
+        },
+
+        methods : {
+            list() {
+
+                // push to database
+                let me = this;
+
+                me.$root.projects().getProjects(function(project) {
+                    console.log(project);
+                }, function(error) {
+                    console.log(error);
+                });
+            },
+
+        }
+    }
+
+
+</script>
