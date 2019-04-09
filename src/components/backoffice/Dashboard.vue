a7dcc3ed1a5fa5189242d896564cdc4679c222d4
Tiago Cruz <tiago.cruz@logon-consulting.de>
2019-04-09 20:35:21 +0100

    moved navbar to bo layout

--- src/components/backoffice/Dashboard.vue
+++ src/components/backoffice/Dashboard.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="app" id="dashboard">
     Dashboard
-    <router-link to="/backoffice/training/list">List Training</router-link>
-    <router-link to="/backoffice/training/create">Create Training</router-link>
-    <router-link to="/backoffice/users/list">List Users</router-link>
-    <router-link to="/backoffice/users/create">Create Users</router-link>
-    <router-link to="/backoffice/project/list">List Projects</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/components/layouts/Backoffice.vue
+++ src/components/layouts/Backoffice.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="backoffice">
     <router-link to="/backoffice">Dashboard</router-link>
+    <router-link to="/backoffice/training/list">List Training</router-link>
+    <router-link to="/backoffice/training/create">Create Training</router-link>
+    <router-link to="/backoffice/users/list">List Users</router-link>
+    <router-link to="/backoffice/users/create">Create Users</router-link>
+    <router-link to="/backoffice/project/list">List Projects</router-link>
+    
     <router-view/>
     
   </div>
