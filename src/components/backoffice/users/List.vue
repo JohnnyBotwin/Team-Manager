98992fbecf4b3f316894629df0bdca0a0ffbe7c2
Tiago Cruz <tiago.cruz@logon-consulting.de>
2019-04-09 21:00:19 +0100

    user roles

--- src/components/backoffice/users/List.vue
+++ src/components/backoffice/users/List.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>user list should be able to delete</div>
+  <div>
+    <h1>Users</h1>
+    <!-- user list should be able to delete -->
+    <template v-if="loading">Loading...</template>
+
+    <template v-if="users !== null">
+      <ul v-for="user in users" :key="user.id">
+        <li>
+          <label>name:</label>
+          <span>{{user.doc.name}}</span>
+        </li>
+        <li>
+          <label>email:</label>
+          <span>{{user.doc.email}}</span>
+        </li>
+        <li>
+          <UserRole :email="user.doc.email"/>
+        </li>
+      </ul>
+    </template>
+  </div>
 </template>
 
 <script>
+import UserRole from "@/components/backoffice/users/Roles";
 export default {
   name: "user-list",
+  components: {
+    UserRole
+  },
+  data() {
+    return {
+      users: null,
+      loading: true
+    };
+  },
   mounted() {
     this.getUsers();
   },
@@ -13,11 +43,15 @@ export default {
       let me = this;
 
       me.$root.users().getUsers(
-        function(users) {
-          console.log(users);
+        function(response) {
+          me.users = response.rows;
+          me.loading = false;
+          console.log(response);
         },
         function(error) {
+          me.loading = false;
           console.log(error);
+          alert(error);
         }
       );
     }
---
+++ src/components/backoffice/users/Roles.vue
@@ -0,0 +1,40 @@
+<template>
+  <div> role: {{role}}</div>
+</template>
+
+<script>
+export default {
+  name: "UserRole",
+  props: {
+    email:null,
+  },
+  data() {
+    return {
+      role: null,
+      loading: true
+    };
+  },
+  mounted() {
+    this.getRole(this.email);
+  },
+  methods: {
+    getRole(email) {
+      let me = this;
+
+      me.$root.userRoles().userRoles(
+        email,
+        function(response) {
+          me.role = response.rows;
+          me.loading = false;
+          console.log(response);
+        },
+        function(error) {
+          me.loading = false;
+          console.log(error);
+          alert(error);
+        }
+      );
+    }
+  }
+};
+</script>
