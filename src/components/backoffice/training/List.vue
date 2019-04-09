1d5f24214f20dbb6e494676eb0fb4d6b31488017
Pedro Costa <pcostarg@gmail.com>
2019-04-09 20:47:47 +0100

    added delete button and button to redirect to create component

--- src/components/backoffice/training/List.vue
+++ src/components/backoffice/training/List.vue
@@ -12,10 +12,11 @@
         <tr v-for="training in trainings" v-bind:key="training.id">
           <td>{{training.doc.title}}</td>
           <td>{{training.doc.description}}</td>
+          <td><a href="#" @click="deleteTraining(training.doc.title)">X</a></td>
         </tr>
       </tbody>
     </table>
-    <!-- <button @click="createTraining">Create Training</button> -->
+    <button @click="createTraining">Create New Training</button>
   </div>
 </template>
 
@@ -46,6 +47,22 @@ export default {
         }
       );
     },
+    deleteTraining(title) {
+      let me = this;
+
+      me.$root.trainings().deleteTraining(title,
+        function(response) {
+          console.log(response)
+          me.listTrainings();
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
+    },
+    createTraining() {
+      this.$router.push('/backoffice/training/create')
+    }
   }
 };
 </script>
