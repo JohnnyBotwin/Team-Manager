aba19236e2f8ed170928d8d47587977e0919c5cb
Pedro Costa <pcostarg@gmail.com>
2019-04-09 19:12:59 +0100

    List and create trainings skeleton

---
+++ src/components/backoffice/training/Create.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="create-training">
+    <h2 class="title">Create Trainings</h2>
+    <label for="project-title">Title</label>
+    <input type="text" name="project-title" id="project-title" v-model="title">
+    <label for="project-description">Description</label>
+    <input type="text" name="project-description" id="project-description" v-model="description">
+    <button @click="createTraining">Create Training</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "BOCreateTraining",
+
+  data() {
+    return {
+      title: "",
+      description: ""
+    };
+  },
+  created() {},
+
+  methods: {
+    createTraining() {
+      let me = this;
+
+      me.$root.trainings().createTraining(
+        {
+          name: me.title,
+          description: me.description
+        },
+        function(response) {
+          console.log(response);
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
+    }
+  }
+};
+</script>
+
+
---
+++ src/components/backoffice/training/List.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="list-trainings">
+    <h2 class="title">List Trainings</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="training in trainings" v-bind:key="training.id">
+          <td>{{training.title}}</td>
+          <td>{{training.description}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <button @click="createTraining">Create Training</button> -->
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "BOListTraining",
+
+  data() {
+    return {
+      trainings: []
+    };
+  },
+  mounted () {
+    this.listTrainings();
+  },
+  created() {},
+
+  methods: {
+    listTrainings() {
+      let me = this;
+
+      me.$root.trainings().getTrainings(
+        function(trainings) {
+          me.trainings = me.getTrainingsFromPouchDBRows(trainings);
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
+    },
+    getTrainingsFromPouchDBRows (trainings){
+      return trainings;
+      // trainings.rows.forEach(element => {
+        
+      // });
+    }
+  }
+};
+</script>
+
+
