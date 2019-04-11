07226b64fb6a2088c604e75e1ab63183b14764d5
André Correia <andrecorreia@parceiro.sonae.pt>
2019-04-11 19:53:49 +0100

    training list

--- src/components/app/Home.vue
+++ src/components/app/Home.vue
@@ -1,7 +1,26 @@
 <template>
 
 	<div class="app" id="home">
- <datepicker></datepicker>
+ 		<datepicker></datepicker>
+
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Title</th>
+					<th scope="col">Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(taining , index) in tainingsList">
+					<th scope="row">{{ index }}</th>
+					<td>{{ taining.doc.title}}</td>
+					<td>{{ taining.doc.description }}</td>
+				</tr>
+			</tbody>
+		</table>
+
+	
 	</div>
 
 </template>
@@ -15,18 +34,37 @@ export default {
     Datepicker
     },
   name : 'Home',
-
+	data (){
+			return {
+				tainingsList : [],
+			}
+	},
 	created() {
 
+		this.listTrainings();
 
 
 	},
 
 	methods : {
 
+		listTrainings(){
+			let me = this;
 
-
+			me.$root.trainings().getTrainings(
+        function(training) {
+					console.log('seucces!!')
+					console.log(training.rows)
+					me.tainingsList = training.rows;
+        },
+        function(error) {
+          console.log('not training found')
+        }
+      );
+		}
 	},
+
+	
 
 }
 
